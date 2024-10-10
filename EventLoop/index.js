a152cd8cd699d60553367924d0b2f88e56a612e5
AryanTapre <taprearyan7@gmail.com>
2024-10-10 23:04:46 +0530

    testing event-loop

---
+++ EventLoop/index.js
@@ -0,0 +1,10 @@
+
+    while(true) {
+        let loopCount = 0;
+        console.log("true "+loopCount);
+        ++loopCount;
+    }
+
+document.getElementById("clickme").addEventListener("click",() => {
+    alert("you clicked a button");
+})
