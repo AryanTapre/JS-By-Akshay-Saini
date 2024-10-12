3f877797d85f695cd190f6cbac779ad9af85eb31
AryanTapre <taprearyan7@gmail.com>
2024-10-13 00:30:29 +0530

    promises-APIs

--- Promises/test.js
+++ Promises/test.js
@@ -1,26 +1,42 @@
-console.log("hello there!");
+// console.log("hello there!");
 
-const promise = new Promise((resolve,reject) => {
-    var error = true;
+// const promise = new Promise((resolve,reject) => {
+//     var error = true;
 
-    setTimeout(() => {
-        console.log("resolving the issue");
-        if(!error) {
+//     setTimeout(() => {
+//         console.log("resolving the issue");
+//         if(!error) {
 
-            resolve("resolved")
-        } 
-        else {
-            reject("error")
-        }
-    },2000);
+//             resolve("resolved")
+//         } 
+//         else {
+//             reject("error")
+//         }
+//     },2000);
+// })
+
+// promise.then((data) => {
+//     console.log(data);
+// })
+// .catch((error)=> {
+//     console.log(error);
+// })
+
+// console.log("ending....");
+
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(() => reject("p1 resolved"),5000);    
 })
 
-promise.then((data) => {
-    console.log(data);
+const p2 = new Promise((resolve,reject) => {
+    setTimeout(() => reject("p2 reject"),2000);
 })
-.catch((error)=> {
-    console.log(error);
+
+const p3 = new Promise((resolve,reject) => {
+    setTimeout(() => reject("p3 resolved"),1000);
 })
 
 
-console.log("ending....");
+Promise.any([p1,p2,p3])
+.then((result) => console.log(result))
+.catch((err) => console.log(err))
